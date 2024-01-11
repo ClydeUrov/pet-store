99820a47255100b890582164b9c0759e02a5d4b4
ClydeUrov <clydeurov@gmail.com>
2024-01-11 16:50:09 +0200

    Update product availability

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,79 +1,88 @@
-import React from 'react';
-import classnames from 'classnames';
-import { usePagination, DOTS } from './usePagination';
-import './pagination.scss';
-const Pagination = props => {
-    const {
-        onPageChange,
-        totalCount,
-        siblingCount = 1,
-        currentPage,
-        pageSize,
-        className
-    } = props;
+import React from "react";
+import classnames from "classnames";
+import { usePagination, DOTS } from "./usePagination";
+import "./pagination.scss";
+const Pagination = (props) => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount = 1,
+    currentPage,
+    pageSize,
+    className,
+  } = props;
 
-    const paginationRange = usePagination({
-        currentPage,
-        totalCount,
-        siblingCount,
-        pageSize
-    });
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  });
 
-    if (currentPage === 0 || paginationRange.length < 2) {
-        return null;
-    }
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null;
+  }
 
-    const onNext = () => {
-        onPageChange(currentPage + 1);
-    };
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
 
-    const onPrevious = () => {
-        onPageChange(currentPage - 1);
-    };
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
 
-    let lastPage = paginationRange[paginationRange.length - 1];
-    return (
-        <div className='pag'>
-            <ul
-                className={classnames('pagination-container', { [className]: className })}
+  let lastPage = paginationRange[paginationRange.length - 1];
+  return (
+    <div className="pag">
+      <ul
+        className={classnames("pagination-container", {
+          [className]: className,
+        })}
+      >
+        <li
+          className={classnames("pagination-item", {
+            disabled: currentPage === 1,
+          })}
+          onClick={onPrevious}
+        >
+          <div className="arrow left" />
+        </li>
+        {paginationRange.map((pageNumber, index) => {
+          if (pageNumber === DOTS) {
+            return (
+              <li key={index} className="pagination-item dots">
+                &#8230;
+              </li>
+            );
+          }
+
+          return (
+            <li
+              key={index}
+              className={classnames("pagination-item", {
+                selected: pageNumber === currentPage,
+              })}
+              onClick={() => onPageChange(pageNumber)}
             >
-                <li
-                    className={classnames('pagination-item', {
-                        disabled: currentPage === 1
-                    })}
-                    onClick={onPrevious}
-                >
-                    <div className="arrow left" />
-                </li>
-                {paginationRange.map((pageNumber, index) => {
-                    if (pageNumber === DOTS) {
-                        return <li key={index} className="pagination-item dots">&#8230;</li>;
-                    }
-
-                    return (
-                        <li
-                            key={index}
-                            className={classnames('pagination-item', { selected: pageNumber === currentPage })}
-                            onClick={() => onPageChange(pageNumber)}
-                        >
-                            {pageNumber}
-                        </li>
-                    );
-                })}
-                <li
-                    className={classnames('pagination-item', {
-                        disabled: currentPage === lastPage
-                    })}
-                    onClick={onNext}
-                >
-                    <div className="arrow right" />
-                </li>
-            </ul>
-            <div>
-                {(currentPage - 1) * pageSize + 1} - {Math.min(currentPage * pageSize, totalCount)} of {totalCount} orders
-            </div>
-        </div>
-    );
+              {pageNumber}
+            </li>
+          );
+        })}
+        <li
+          className={classnames("pagination-item", {
+            disabled: currentPage === lastPage,
+          })}
+          onClick={onNext}
+        >
+          <div className="arrow right" />
+        </li>
+      </ul>
+      <div>
+        {(currentPage - 1) * pageSize + 1} -{" "}
+        {Math.min(currentPage * pageSize, totalCount)} of {totalCount} products
+      </div>
+    </div>
+  );
 };
 
 export default Pagination;
