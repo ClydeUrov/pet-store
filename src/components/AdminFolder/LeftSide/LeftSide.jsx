013b841512ce1e08f0ca8a31838818fb4ffdbc90
ClydeUrov <clydeurov@gmail.com>
2024-02-14 13:03:44 +0200

    New

--- src/components/AdminFolder/LeftSide/LeftSide.jsx
+++ src/components/AdminFolder/LeftSide/LeftSide.jsx
@@ -44,12 +44,12 @@ const LeftSide = () => {
   return (
     <div>
       <NavLink to="/" className={css.logo}>
-        {constants && constants.length && constants[0].value !== null && constants[0].value?.filePath ? (
-          <img
-            src={constants[0].value.filePath}
-            alt={constants[0]?.key}
-          />
-        ) : ""}
+      {constants && constants.length && constants[0].value && constants[0].value !== null && constants[0].value?.filePath ? (
+        <img
+          src={constants[0].value.filePath}
+          alt={constants[0]?.key}
+        />
+      ) : ""}
       </NavLink>
       <div className={css.list}>
         {navItems.map(({ href, text }) => (
